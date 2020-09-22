b7cb40c801fc95873abc1d9d22050160e12002ef
junglq <junglqq@gmail.com>
2020-09-22 21:47:09 +0300

    we

--- js/app.js
+++ js/app.js
@@ -32,17 +32,17 @@ testWebP(function (support) {
 	}
 });
 */
-function ibg() {
-	if (isIE()) {
-		let ibg = document.querySelectorAll("._ibg");
-		for (var i = 0; i < ibg.length; i++) {
-			if (ibg[i].querySelector('img') && ibg[i].querySelector('img').getAttribute('src') != null) {
-				ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
-			}
-		}
-	}
-}
-ibg();
+// function ibg() {
+// 	if (isIE()) {
+// 		let ibg = document.querySelectorAll("._ibg");
+// 		for (var i = 0; i < ibg.length; i++) {
+// 			if (ibg[i].querySelector('img') && ibg[i].querySelector('img').getAttribute('src') != null) {
+// 				ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+// 			}
+// 		}
+// 	}
+// }
+// ibg();
 
 if (document.querySelector('.wrapper')) {
 	document.querySelector('.wrapper').classList.add('_loaded');
