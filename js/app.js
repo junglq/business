32bcc74904bfdc677802c1bbac23460f4f1c32f3
junglq <junglqq@gmail.com>
2020-09-26 01:37:09 +0300

    sa

--- js/app.js
+++ js/app.js
@@ -111,44 +111,10 @@ if (location.hash) {
 }
 //=================
 //Menu
-let iconMenu = document.querySelector(".menu-header__icon");
-if (iconMenu != null) {
-	let delay = 100;
-	let menuBody = document.querySelector(".menu-header__menu");
-	iconMenu.addEventListener("click", function (e) {
-		if (unlock) {
-			body_lock(delay);
-			iconMenu.classList.toggle("active");
-			menuBody.classList.toggle("active");
-		}
-	});
-};
-function menu_close() {
-	let iconMenu = document.querySelector(".menu-header__icon");
-	let menuBody = document.querySelector(".menu-header__menu");
-	iconMenu.classList.remove("active");
-	menuBody.classList.remove("active");
-}
-//=================
-//BodyLock
-function body_lock(delay) {
-	let body = document.querySelector("body");
-	if (body.classList.contains('lock')) {
-		body_lock_remove(delay);
-	} else {
-		body_lock_add(delay);
-	}
-}
-function body_lock_remove(delay) {
-	let body = document.querySelector("body");
-	body.classList.remove("lock");
-
-}
-
-function body_lock_add(delay) {
-	let body = document.querySelector("body");
-	body.classList.add("lock");
-}
+$('.menu-header__icon').on('click', function () {
+	$('.menu-header__icon,.menu-header__menu').toggleClass('active');
+	$('body').toggleClass('lock');
+})
 //=================
 
 // LettersAnimation
